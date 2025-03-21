83ddeef30b2af6f70f05b0481540e111f6d6277d
viethoang1520 <viethoang1520.dev@gmail.com>
2025-03-21 23:12:53 +0700

    feat: update button's props

--- frontend/src/Components/Cart/Cart.jsx
+++ frontend/src/Components/Cart/Cart.jsx
@@ -8,10 +8,20 @@ export default function Cart() {
       <div className="button-block">
         <Button
           content="THANH TOÁN"
-          width={540}
+          width={420}
           height={45}
           textColor="#fff"
           backgroundColor="red"
+        />
+      </div>
+      <div className="second-block">
+        <Button
+          content="Giỏ hàng"
+          height={40}
+          width={420}
+          textColor="#3b3b3c"
+          backgroundColor="#f3f5f6"
+          borderRadius={3}
         />
       </div>
     </div>
--- frontend/src/Components/common/Button/Button.jsx
+++ frontend/src/Components/common/Button/Button.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Button.scss'  
-export default function Button({content, width, height, textColor, backgroundColor}) {
+export default function Button({content, width, height, textColor, backgroundColor, borderRadius, border, onClick}) {
   return (
     <div style={{
       width: `${width}px`,
       height: `${height}px`,
       backgroundColor: backgroundColor,
+      borderRadius: `${borderRadius}px`,
+      border: border,
     }}
-      className='button-container' >
+      onClick={onClick}
+      className='button-container'>
       <span style={{color: textColor}} className='button-content'>{ content }</span>
     </div>
   )
